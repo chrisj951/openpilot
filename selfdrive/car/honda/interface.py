82c03ea1cf85ecc7e36f1d849b615d424375d64a
Comma Device <device@comma.ai>
2019-07-16 14:58:46 -0400

    update

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -176,7 +176,7 @@ class CarInterface(object):
     ret.lateralTuning.pid.reactMPC = -0.05
     ret.lateralTuning.pid.rateFFGain = 0.4
 
-    ret.lateralTuning.pid.polyFactor = 0.003
+    ret.lateralTuning.pid.polyFactor = 0.0015
     ret.lateralTuning.pid.polyDampTime = 0.2
     ret.lateralTuning.pid.polyReactTime = 2.0
     ret.lateralTuning.pid.polyScale = [[0.0, 0.5, 1.0, 2.0, 5.0], [1.0, 0.5, 0.25, 0.1, 0.0], [1.0, 1.0, 1.0, 1.0, 1.0]]  # [abs rate, scale UP, scale DOWN]
@@ -233,8 +233,8 @@ class CarInterface(object):
       ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
       ret.longitudinalTuning.kiBP = [0., 35.]
       ret.longitudinalTuning.kiV = [0.18, 0.12]  
-      ret.lateralTuning.pid.dampTime = 0.02
-      ret.lateralTuning.pid.reactMPC = 0.0
+      ret.lateralTuning.pid.dampTime = 0.01
+      ret.lateralTuning.pid.reactMPC = -0.05
       ret.lateralTuning.pid.rateFFGain = 0.4
       ret.steerLimitAlert = False
 
--- selfdrive/ui/ui.c
+++ selfdrive/ui/ui.c
@@ -2137,15 +2137,6 @@ void handle_message(UIState *s, void *which) {
     struct cereal_ControlsState_LateralPIDState pdata;
     cereal_read_ControlsState_LateralPIDState(&pdata, datad.lateralControlState.pidState);
 
-    struct cereal_GpsLocationData datag;
-    cereal_read_GpsLocationData(&datag, eventd.gpsLocation);
-    s->scene.gpsAccuracy = datag.accuracy;
-    if (s->scene.gpsAccuracy > 100) {
-      s->scene.gpsAccuracy = 99.99;
-    } else if (s->scene.gpsAccuracy < 0) {
-      s->scene.gpsAccuracy = 99.8;
-    }
-
     if (datad.vCruise != s->scene.v_cruise) {
       s->scene.v_cruise_update_ts = eventd.logMonoTime;
     }
@@ -2438,7 +2429,7 @@ static void ui_update(UIState *s) {
     s->alert_blinked = false;
   }
 
-  zmq_pollitem_t polls[13] = {{0}};
+  zmq_pollitem_t polls[12] = {{0}};
   // Wait for next rgb image from visiond
   while(true) {
     assert(s->ipc_fd >= 0);
@@ -2552,6 +2543,38 @@ static void ui_update(UIState *s) {
         polls[8].revents || polls[9].revents || polls[plus_sock_num].revents) {
       // awake on any (old) activity
       set_awake(s, true);
+    }
+
+    if (polls[9].revents) {
+      //gps socket
+      zmq_msg_t msg;
+      err = zmq_msg_init(&msg);
+      assert(err == 0);
+      err = zmq_msg_recv(&msg, s->gps_sock_raw, 0);
+      assert(err >= 0);
+
+      struct capn ctx;
+      capn_init_mem(&ctx, zmq_msg_data(&msg), zmq_msg_size(&msg), 0);
+
+      cereal_Event_ptr eventp;
+      eventp.p = capn_getp(capn_root(&ctx), 0, 1);
+      struct cereal_Event eventd;
+      cereal_read_Event(&eventd, eventp);
+
+      struct cereal_GpsLocationData datad;
+      cereal_read_GpsLocationData(&datad, eventd.gpsLocationExternal);
+
+      s->scene.gpsAccuracy = datad.accuracy;
+      if (s->scene.gpsAccuracy > 100)
+      {
+        s->scene.gpsAccuracy = 99.99;
+      }
+      else if (s->scene.gpsAccuracy < 0.01)
+      {
+        s->scene.gpsAccuracy = 99.8;
+      }
+      capn_free(&ctx);
+      zmq_msg_close(&msg);
     }
 
     if (polls[plus_sock_num].revents) {
