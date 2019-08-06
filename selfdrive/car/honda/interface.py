2475a1992190912c6750022874022919b27763bb
Comma Device <device@comma.ai>
2019-08-05 21:38:21 -0400

    oops

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -236,12 +236,12 @@ class CarInterface(object):
       ret.longitudinalTuning.kiBP = [0., 35.]
       ret.longitudinalTuning.kiV = [0.18, 0.12]
       ret.lateralTuning.pid.rateFFGain = 0.4
-      ret.lateralTuning.pid.polyFactor = 0.002
+      ret.lateralTuning.pid.polyFactor = 0.001
       ret.lateralTuning.pid.polyDampTime = 0.05
       ret.lateralTuning.pid.polyReactTime = 0.5
-      ret.lateralTuning.pid.dampTime = 0.01
+      ret.lateralTuning.pid.dampTime = 0.1
       ret.lateralTuning.pid.dampMPC = 0.1
-      ret.lateralTuning.pid.reactMPC = 0.02
+      ret.lateralTuning.pid.reactMPC = 0.025
       ret.steerLimitAlert = False
 
     elif candidate == CAR.ACURA_ILX:
