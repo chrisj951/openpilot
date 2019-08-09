f134b9f0290368536edabaa554de361e311d65aa
Comma Device <device@comma.ai>
2019-08-08 22:02:48 -0400

    update pid

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -230,7 +230,7 @@ class CarInterface(object):
       ret.centerToFront = ret.wheelbase * 0.39
       ret.steerRatio = 15  # 12.58 is spec end-to-end
       tire_stiffness_factor = 0.82
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.18]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.8], [0.18]]
       ret.longitudinalTuning.kpBP = [0., 5., 35.]
       ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
       ret.longitudinalTuning.kiBP = [0., 35.]
@@ -241,7 +241,7 @@ class CarInterface(object):
       ret.lateralTuning.pid.polyReactTime = 0.5
       ret.lateralTuning.pid.dampTime = 0.0
       ret.lateralTuning.pid.dampMPC = 0.1
-      ret.lateralTuning.pid.reactMPC = 0.025
+      ret.lateralTuning.pid.reactMPC = 0.005
       ret.steerLimitAlert = False
 
     elif candidate == CAR.ACURA_ILX:
