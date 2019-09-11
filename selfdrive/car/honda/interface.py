1e9326a8b9e328fe1bb92588e6d9b10c866b4973
Comma Device <device@comma.ai>
2019-09-11 11:43:15 -0400

    update pid to live tune and interface

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -236,9 +236,9 @@ class CarInterface(object):
       ret.mass = 2987. * CV.LB_TO_KG + STD_CARGO_KG
       ret.wheelbase = 2.7
       ret.centerToFront = ret.wheelbase * 0.39
-      ret.steerRatio = 15  # 12.58 is spec end-to-end
-      tire_stiffness_factor = 0.82
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.5], [0.2]]
+      ret.steerRatio = 15.  # 12.58 is spec end-to-end
+      tire_stiffness_factor = 1.
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.485], [0.175]]
       ret.longitudinalTuning.kpBP = [0., 5., 35.]
       ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
       ret.longitudinalTuning.kiBP = [0., 35.]
--- selfdrive/controls/lib/latcontrol_pid.py
+++ selfdrive/controls/lib/latcontrol_pid.py
@@ -2,14 +2,25 @@ from selfdrive.controls.lib.pid import PIController
 from selfdrive.controls.lib.drive_helpers import get_steer_max
 from cereal import car
 from cereal import log
+from selfdrive.kegman_conf import kegman_conf
 
 
 class LatControlPID(object):
   def __init__(self, CP):
+    kegman_conf(CP)
     self.pid = PIController((CP.lateralTuning.pid.kpBP, CP.lateralTuning.pid.kpV),
                             (CP.lateralTuning.pid.kiBP, CP.lateralTuning.pid.kiV),
                             k_f=CP.lateralTuning.pid.kf, pos_limit=1.0)
     self.angle_steers_des = 0.
+
+  def live_tune(self, CP):
+    self.frame += 1
+    if self.frame % 300 == 0:
+      # live tuning through /data/openpilot/tune.py overrides interface.py settings
+      kegman = kegman_conf()
+      self.pid._k_i = ([0.], [float(kegman.conf['Ki'])])
+      self.pid._k_p = ([0.], [float(kegman.conf['Kp'])])
+      self.pid.k_f = (float(kegman.conf['Kf']))
 
   def reset(self):
     self.pid.reset()
@@ -18,6 +29,8 @@ class LatControlPID(object):
     pid_log = log.ControlsState.LateralPIDState.new_message()
     pid_log.steerAngle = float(angle_steers)
     pid_log.steerRate = float(angle_steers_rate)
+
+    self.live_tune(CP)
 
     if v_ego < 0.3 or not active:
       output_steer = 0.0
