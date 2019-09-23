786d13348a946e36563801f540c8f2b9c62bdeb9
Comma Device <device@comma.ai>
2019-09-23 16:28:24 -0400

    fix need to f fix kf tuning later some weird error with float and non-int

--- selfdrive/controls/lib/latcontrol_pid.py
+++ selfdrive/controls/lib/latcontrol_pid.py
@@ -4,6 +4,7 @@ from cereal import car
 from cereal import log
 from selfdrive.kegman_conf import kegman_conf
 
+
 class LatControlPID(object):
   def __init__(self, CP):
     self.kegman = kegman_conf(CP)
@@ -12,31 +13,33 @@ class LatControlPID(object):
                             (CP.lateralTuning.pid.kiBP, CP.lateralTuning.pid.kiV),
                             k_f=CP.lateralTuning.pid.kf, pos_limit=1.0)
     self.angle_steers_des = 0.
-    self.frame = 0.
-
-  def live_tune(self, CP):
-    self.frame += 1
-    if self.frame % 300 == 0:
-      # live tuning through /data/openpilot/tune.py overrides interface.py settings
-      self.kegman = kegman_conf()
-      self.steerKiV = ([0.], [float(self.kegman.conf['Ki'])])
-      self.steerKpV = ([0.], [float(self.kegman.conf['Kp'])])
-      self.steerKf = (float(self.kegman.conf['Kf']))
-      self.deadzone = float(self.kegman.conf['deadzone'])
-      self.pid = PIController((CP.lateralTuning.pid.kpBP, self.steerKpV),
-                          (CP.lateralTuning.pid.kiBP, self.steerKiV),
-                          k_f=self.steerKf, pos_limit=1.0)
-      self.frame = 0
+    self.mpc_frame = 0
 
   def reset(self):
     self.pid.reset()
+    
+  def live_tune(self, CP):
+    self.mpc_frame += 1
+    if self.mpc_frame % 300 == 0:
+      # live tuning through /data/openpilot/tune.py overrides interface.py settings
+      self.kegman = kegman_conf()
+      self.steerKpV = [float(self.kegman.conf['Kp'])]
+      self.steerKiV = [float(self.kegman.conf['Ki'])]
+      self.pid = PIController((CP.lateralTuning.pid.kpBP, self.steerKpV),
+                          (CP.lateralTuning.pid.kiBP, self.steerKiV),
+                           k_f=CP.lateralTuning.pid.kf, pos_limit=1.0)
+      self.deadzone = float(self.kegman.conf['deadzone'])
+        
+      self.mpc_frame = 0    
 
+      
   def update(self, active, v_ego, angle_steers, angle_steers_rate, eps_torque, steer_override, CP, VM, path_plan):
+    
+    self.live_tune(CP)
+    
     pid_log = log.ControlsState.LateralPIDState.new_message()
     pid_log.steerAngle = float(angle_steers)
     pid_log.steerRate = float(angle_steers_rate)
-
-    self.live_tune(CP)
 
     if v_ego < 0.3 or not active:
       output_steer = 0.0
@@ -53,7 +56,9 @@ class LatControlPID(object):
         # TODO: feedforward something based on path_plan.rateSteers
         steer_feedforward -= path_plan.angleOffset   # subtract the offset, since it does not contribute to resistive torque
         steer_feedforward *= v_ego**2  # proportional to realigning tire momentum (~ lateral accel)
-      deadzone = self.deadzone
+      
+      deadzone = self.deadzone    
+        
       output_steer = self.pid.update(self.angle_steers_des, angle_steers, check_saturation=(v_ego > 10), override=steer_override,
                                      feedforward=steer_feedforward, speed=v_ego, deadzone=deadzone)
       pid_log.active = True
