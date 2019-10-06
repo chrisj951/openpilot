d81d9664f81fd5cd715d39251e654071c7f93c32
Comma Device <device@comma.ai>
2019-10-06 14:02:43 -0400

    update

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -233,13 +233,13 @@ class CarInterface(object):
 
     elif candidate == CAR.INSIGHT:
       stop_and_go = True
-      ret.mass = 2987. * CV.LB_TO_KG + STD_CARGO_KG
+      ret.mass = 3000. * CV.LB_TO_KG + STD_CARGO_KG
       ret.wheelbase = 2.7
       ret.centerToFront = ret.wheelbase * 0.39
-      ret.steerRatio = 12.58
-      tire_stiffness_factor = 0.82
-      ret.lateralTuning.pid.kf = 0.00000
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.45], [0.21]]
+      ret.steerRatio = 13.82
+      tire_stiffness_factor = 0.72
+      ret.lateralTuning.pid.kf = 0.00006
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.18]]
       ret.longitudinalTuning.kpBP = [0., 5., 35.]
       ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
       ret.longitudinalTuning.kiBP = [0., 35.]
@@ -435,7 +435,7 @@ class CarInterface(object):
     ret.stoppingControl = True
     ret.startAccel = 0.5
 
-    ret.steerActuatorDelay = 0.1
+    ret.steerActuatorDelay = 1.0
     ret.steerRateCost = 0.5
 
     return ret
